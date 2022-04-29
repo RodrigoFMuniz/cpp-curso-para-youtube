e6e97267a91de1e0910c2bec88ecbc7a48b5626e
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-29 02:14:34 -0300

    Aula 02, tipos

---
+++ aula02/tipos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numero;// 10  , 20
+    char letra; // 'R'
+    char letras[20]; //Passa a ser um vetor
+    double decimal;// 10.399999999999999
+    float flutuante; // 3.5
+    bool alive; // true ou false
+    string nome = "Codevibes"; // "Codevibes"
+
+    cout << nome << endl;
+
+    return 0;
+}
