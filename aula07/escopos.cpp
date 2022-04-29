cb41b40a56d125af900a58672b0061e157a51258
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-29 02:46:36 -0300

    Aula 07, Escopos

---
+++ aula07/escopos.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int n1 = 10,n2; // variáveis de escopo global.
+
+int main(){
+    int n2,n3; // variáveis locais
+
+    cout << n1 <<endl;
+    return 0;
+}
