96154fdcefe054f784a04a0a7dc1941ee85cd230
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-29 02:28:48 -0300

    Aula 04, comentário

---
+++ aula04/comentarios.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // Este é um comentário em linha
+    /*
+        Este é um comentário de várias
+        linhas.
+        Como é possível entender agora.
+    */
+    return 0;
+}
