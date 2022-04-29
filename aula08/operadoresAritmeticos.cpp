00f39bdbf646dc5146411b3d155b6af669c071a8
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-29 20:18:04 -0300

    Aula 08, operadores aritmeticos, divisão com numerador ponto flutuante

--- aula08/operadoresAritmeticos.cpp
+++ aula08/operadoresAritmeticos.cpp
@@ -11,12 +11,14 @@ int main(){
     int multiplicacao = 0;
     int divisao = 0;
     int divisao2 = 0;
+    int divisaoComNumeradorFlutuante = 0;
 
     soma = n1 + n2;
     subtracao = n1 - n2;
     multiplicacao = n1 * n2;
     divisao = n1 / n2;
     divisao2 = n1 / 3.4;
+    divisaoComNumeradorFlutuante = 4.5 / 3;
 
     cout << "Soma: " << soma << endl;
     cout << "Subtração: " << subtracao << endl;
@@ -25,6 +27,9 @@ int main(){
 
     /* divisão incorreta, por erro de tipagem, os pontos flutuantes são ignorados*/
     cout << "Divisão com erro devido a tipagem: " << divisao2 << endl; // retorna 2
+
+    /* divisão incorreta, por erro de tipagem, os pontos flutuantes são ignorados*/
+    cout << "Divisão com erro devido a tipagem no numerador: " << divisaoComNumeradorFlutuante << endl; // retorna 1
 
     return 0;
 }
