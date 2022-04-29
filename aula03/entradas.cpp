f6dbc519e30d58f670eb2fbd51e09891e6b78b57
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-29 02:23:02 -0300

    Aula 03, entradas

---
+++ aula03/entradas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string nome; // "Codevibes"
+
+    cout << "Digite o nome: ";
+    cin >> nome;
+
+
+    cout << nome << endl;
+
+    return 0;
+}
