d297d30bd2b9c4abb92d0b9906e5456bfb4d9557
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-29 20:04:58 -0300

    Aula 08, operadores aritmeticos, subtração

--- aula08/operadoresAritmeticos.cpp
+++ aula08/operadoresAritmeticos.cpp
@@ -7,10 +7,13 @@ int n1 = 10,n2 = 5;
 int main(){
 
     int soma = 0;
+    int subtracao = 0;
 
     soma = n1 + n2;
+    subtracao = n1 - n2;
 
     cout << "Soma: " << soma << endl;
+    cout << "Subtração: " << subtracao << endl;
 
     return 0;
 }
