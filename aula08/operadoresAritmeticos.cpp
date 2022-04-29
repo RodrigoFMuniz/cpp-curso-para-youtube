e32fec9db73f299fa5c7ff32a239340648a66479
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-29 20:13:20 -0300

    Aula 08, operadores aritmeticos, divisão

--- aula08/operadoresAritmeticos.cpp
+++ aula08/operadoresAritmeticos.cpp
@@ -9,14 +9,19 @@ int main(){
     int soma = 0;
     int subtracao = 0;
     int multiplicacao = 0;
+    int divisao = 0;
+    int divisao2 = 0;
 
     soma = n1 + n2;
     subtracao = n1 - n2;
     multiplicacao = n1 * n2;
+    divisao = n1 / n2;
+    divisao2 = n1 / 3.4;
 
     cout << "Soma: " << soma << endl;
     cout << "Subtração: " << subtracao << endl;
     cout << "Multiplicação: " << multiplicacao << endl;
+    cout << "Divisão: " << divisao << endl; // divisão correta, sem erros
 
     return 0;
 }
