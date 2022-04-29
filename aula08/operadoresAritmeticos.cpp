bdcdcdb851d5a0ea8859d89aff585c21a41c6af7
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-04-29 20:03:43 -0300

    Aula 08, operadores aritmeticos, soma

---
+++ aula08/operadoresAritmeticos.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int n1 = 10,n2 = 5;
+
+int main(){
+
+    int soma = 0;
+
+    soma = n1 + n2;
+
+    cout << "Soma: " << soma << endl;
+
+    return 0;
+}
